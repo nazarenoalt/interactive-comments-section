ebe9331d88918750f6f812a1f3fe473bfa27dc41
Nazareno Altamiranda <nazareno.anzar@gmail.com>
2025-01-06 18:59:31 -0300

    add css variables

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -2,7 +2,22 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
   :root {
-    
+    /* Primary Colors */
+    --moderate-blue: hsl(238, 40%, 52%);
+    --soft-red: hsl(358, 79%, 66%);
+    --light-grayish-blue: hsl(239, 57%, 85%);
+    --pale-red: hsl(357, 100%, 86%);
+
+    /* Neutral Colors */
+    --dark-blue: hsl(212, 24%, 26%);
+    --grayish-blue: hsl(211, 10%, 45%);
+    --light-gray: hsl(223, 19%, 93%);
+    --very-light-gray: hsl(228, 33%, 97%);
+    --white: hsl(0, 0%, 100%);
+
+    /* Sizes */
+    --desktop: 1440px;
+    --mobile: 375px;
   }
   *, *::before, *::after {
     box-sizing: border-box;

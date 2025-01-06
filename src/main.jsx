25583dd84f0ce650eda35495c57a233731e04ea2
Nazareno Altamiranda <nazareno.anzar@gmail.com>
2025-01-06 19:03:15 -0300

    fix: wrong implementation of globalStyles

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GlobalStyles>
       <App />
-    </GlobalStyles>
+      <GlobalStyles />
   </StrictMode>,
 )
